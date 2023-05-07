468132f5a2c02be9225c962d539ad657049d30d3
haticeturann <104205981+haticeturann@users.noreply.github.com>
2023-05-07 06:03:17 +0300

    login sayfası eklendi

--- route/user.js
+++ route/user.js
@@ -1,10 +1,36 @@
 const express = require("express");
 const route = express.Router();
 
-route.post("/user",(req,res)=>{
-    const{username,password} = req.body;
 
-    res.send("Hoşgeldiniz :" + username )
+
+
+
+module.exports=route;
+
+route.post("/user",(req,res)=>{
+
+const{username,password} = req.body;
+
+const isim ="hatice";
+const sifre="123";
+if(username===isim&&sifre===password)
+{
+ res.send("Hoşgeldiniz :" + username )
+}
+else{
+res.send("Bilgilerinizi Kontrol Ediniz")
+ }
+
     
 })
+route.get("/goster",(req,res)=>{
+
+const data={
+         
+isim:"hatice",password:"123"
+}
+res.send(data);
+})
+
+module.exports=route;
 
